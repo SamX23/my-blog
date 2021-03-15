fb810ea547e871877377c7ef964e585f55badd8d
samx23 <skalexsong@gmail.com>
2021-03-15 23:13:28 +0700

    use styled component on footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,32 @@
-import footer from "../styles/footer.module.css";
+import { makeStyles, createStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
 
 export default function Footer() {
+  const useStyles = makeStyles({
+    footer_Bar: {
+      borderTop: "0.2em solid #e9e0ff",
+      paddingTop: "2em",
+      margin: "auto",
+      alignContent: "space-around",
+    },
+  });
+
+  const classes = useStyles();
   return (
-    <footer className={footer.footer}>
-      <p>
-        Powered by Next - Created by
-        <a href="https://kalamallah.xyz"> Sami</a>
-      </p>
+    <footer>
+      <Grid className={classes.footer_Bar} container>
+        <Grid>
+          Ngoding with{" "}
+          <span role="img" aria-label="love">
+            ♥️
+          </span>{" "}
+          by <a href="https://kalamallah.xyz"> Sami</a>
+        </Grid>
+
+        {/* <Grid item>
+          <a href="https://web.facebook.com/SamiKalamallah">Facebook</a>
+        </Grid> */}
+      </Grid>
     </footer>
   );
 }
