1918cdef5b71beff646e30dfe942bda512987441
samx23 <skalexsong@gmail.com>
2021-03-16 23:49:22 +0700

    add css cleaner for ssr support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import { AppProps } from "next/app";
+import { useEffect } from "react";
 import "../styles/global.css";
 
 function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
