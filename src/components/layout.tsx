e4b95ddd5ec425b1ce22af3f9946ee13f4150737
samx23 <skalexsong@gmail.com>
2021-03-18 00:54:48 +0700

    refactor head and back component

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,50 +1,30 @@
-import Head from "next/head";
-import Link from "next/link";
+import HeadTag from "./head";
 import styles from "../../styles/layout.module.css";
 import Hero from "./hero";
+import BackToHome from "./backToHome";
 import Footer from "./footer";
-
-const name = "Sami Kalammallah";
-export const siteTitle = "Sami Kalammallah";
+import { defaultTitle, profile } from "../mock/data";
 
 export default function Layout({
   children,
   home,
+  siteTitle = defaultTitle,
 }: {
   children: React.ReactNode;
   home?: boolean;
+  siteTitle?: string;
 }) {
   return (
     <div className={styles.container}>
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Sami Kalammallah blog about his front end journey"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
-      </Head>
+      <HeadTag siteTitle={siteTitle} />
 
       <header className={styles.header}>
-        <Hero home={home} name={name} />
+        <Hero home={home} name={profile.fullName} />
       </header>
 
       <main>{children}</main>
 
-      {!home && (
-        <div className={styles.backToHome}>
-          <Link href="/">
-            <a>← Back to home</a>
-          </Link>
-        </div>
-      )}
+      {!home && <BackToHome />}
 
       <Footer />
     </div>
