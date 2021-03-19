05f207f089dee8f2c05369c4416c2b8f7565ee87
samx23 <skalexsong@gmail.com>
2021-03-19 23:31:25 +0700

    create theme toggle sample

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -1,15 +1,18 @@
+import { Button } from "@material-ui/core";
+import Tooltip from "@material-ui/core/Tooltip";
 import Link from "next/link";
+import { useContext } from "react";
+import WbSunnyIcon from "@material-ui/icons/WbSunny";
+import Brightness3Icon from "@material-ui/icons/Brightness3";
 import navigation from "../../styles/navigation.module.css";
+import { ToggleThemeContext } from "../theme/themeProvider";
 
 export default function Navigation() {
+  const { toggleTheme, isDark } = useContext(ToggleThemeContext);
+
   return (
     <nav className={navigation.nav__Bar}>
       <ul className={navigation.nav__Menu}>
-        <Link href="/">
-          <a>
-            <li>Home</li>
-          </a>
-        </Link>
         <Link href="/github">
           <a>
             <li>Github</li>
@@ -20,6 +23,11 @@ export default function Navigation() {
             <li>Blog</li>
           </a>
         </Link>
+        <Tooltip title="Toggle Theme">
+          <Button variant="text" onClick={toggleTheme}>
+            {isDark ? <WbSunnyIcon /> : <Brightness3Icon />}
+          </Button>
+        </Tooltip>
       </ul>
     </nav>
   );
