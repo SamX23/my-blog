4f6a60b9427095c6296c7ecbe2ea1393b5bdbfd7
samx23 <skalexsong@gmail.com>
2021-03-21 21:51:23 +0700

    refactor using styled component and change wording

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,34 @@
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
 import Layout from "../src/components/layout";
 import Navigation from "../src/components/navigation";
-import utilStyles from "../styles/utils.module.css";
+
+const useStyles = makeStyles({
+  typography: {
+    padding: "1em 0",
+    lineHeight: "2em",
+  },
+  container: {
+    maxWidth: "45em",
+    margin: "auto",
+  },
+});
 
 export default function Home() {
+  const classes = useStyles();
   return (
     <Layout home>
       <Navigation />
-      <section className={utilStyles.headingMd && utilStyles.container}>
-        <p>
-          A front-end web developer as hobbiest (yet), this blog is created
-          using <a href="https://nextjs.org/">Next</a> framework and{" "}
-          <a href="https://www.typescriptlang.org/">Typescript</a>.
-        </p>
-        <p>
-          Or you can see me doing code on my github{" "}
-          <a href="https://github.com/SamX23">Github Profile</a>, while living
-          in social network on{" "}
-          <a href="https://facebook.com/samikalamallah">facebook</a>.
-        </p>
+      <section className={classes.container}>
+        <Typography variant="h6" className={classes.typography}>
+          A front-end web developer as hobbiest (yet), focusing on React tech
+          stack and has developed some personal project on github. This blog is
+          also created using <a href="https://nextjs.org/">Next</a> framework
+          and <a href="https://www.typescriptlang.org/">Typescript</a>. You can
+          see my project code on{" "}
+          <a href="https://github.com/SamX23">Github Profile</a>. Thank you for
+          coming 🙂
+        </Typography>
       </section>
     </Layout>
   );
