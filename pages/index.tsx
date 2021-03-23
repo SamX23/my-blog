9b60c9c366060c2def5b6aa59c504302a5e938a4
samx23 <skalexsong@gmail.com>
2021-03-23 20:30:35 +0700

    separate navigation from layout on homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,19 +17,22 @@ const useStyles = makeStyles({
 export default function Home() {
   const classes = useStyles();
   return (
-    <Layout home>
+    <>
       <Navigation />
-      <section className={classes.container}>
-        <Typography variant="h6" className={classes.typography}>
-          A front-end web developer as hobbiest (yet), focusing on React tech
-          stack and has developed some personal project on github. This blog is
-          also created using <a href="https://nextjs.org/">Next</a> framework
-          and <a href="https://www.typescriptlang.org/">Typescript</a>. You can
-          see my project code on{" "}
-          <a href="https://github.com/SamX23">Github Profile</a>. Thank you for
-          coming 🙂
-        </Typography>
-      </section>
-    </Layout>
+      <Layout home>
+        <section className={classes.container}>
+          <Typography variant="h6" className={classes.typography}>
+            A front-end web developer as hobbiest (yet), focusing on React tech
+            stack and has developed some personal project on github. This blog
+            is also created using <a href="https://nextjs.org/">Next</a>{" "}
+            framework and{" "}
+            <a href="https://www.typescriptlang.org/">Typescript</a>. You can
+            see my project code on{" "}
+            <a href="https://github.com/SamX23">Github Profile</a>. Thank you
+            for coming 🙂
+          </Typography>
+        </section>
+      </Layout>
+    </>
   );
 }
