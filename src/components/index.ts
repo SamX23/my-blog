edbf7662f87539c31f36e67cc16acc7dc8ad1e4a
samx23 <skalexsong@gmail.com>
2021-04-16 00:54:40 +0700

    Create : index exporter

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { default as HeadTag } from "./head";
+export { default as Navigation } from "./navigation";
+export { default as Hero } from "./hero";
+export { default as BackToHome } from "./backToHome";
+export { default as Footer } from "./footer";
+export { default as themeButton } from "./themeButton";
+export { default as ProgressBar } from "./progressBar";
+export { default as ThemeButton } from "./themeButton";
