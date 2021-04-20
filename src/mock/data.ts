c04aedff504aab7ac2746d1282c87cb52681b246
samx23 <skalexsong@gmail.com>
2021-04-20 07:00:15 +0700

    Update : add project data

--- src/mock/data.ts
+++ src/mock/data.ts
@@ -42,3 +42,52 @@ export const socialMedia = {
     },
   ],
 };
+
+export const project = {
+  githubLink: (repo, theme) =>
+    `https://github-readme-stats.vercel.app/api/pin/?username=samx23&repo=${repo}&theme=${theme}`,
+  repo: [
+    {
+      id: nanoid(),
+      name: "Random Ayah Generator",
+      repo: "read-random-ayah",
+      link: "https://github.com/SamX23/read-random-ayah",
+    },
+    {
+      id: nanoid(),
+      name: "Personal Website",
+      repo: "my-blog",
+      link: "https://github.com/SamX23/my-blog",
+    },
+    {
+      id: nanoid(),
+      name: "Group Chat App",
+      repo: "group-chat-app",
+      link: "https://github.com/SamX23/group-chat-app",
+    },
+    {
+      id: nanoid(),
+      name: "Simple movie catalog",
+      repo: "moviedb-web-app",
+      link: "https://github.com/SamX23/moviedb-web-app",
+    },
+    {
+      id: nanoid(),
+      name: "Pokedex App",
+      repo: "pokedex-dicoding",
+      link: "https://github.com/SamX23/pokedex-dicoding",
+    },
+    {
+      id: nanoid(),
+      name: "Google Clone",
+      repo: "search-engine",
+      link: "https://github.com/SamX23/search-engine",
+    },
+    {
+      id: nanoid(),
+      name: "Hangman Python",
+      repo: "hangman-overhaul",
+      link: " https://github.com/SamX23/hangman-overhaul",
+    },
+  ],
+};
