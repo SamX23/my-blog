a81fa07785a36d3205734318f62b965e25fa9c04
samx23 <skalexsong@gmail.com>
2021-04-20 13:36:44 +0700

    Create : add scroll to top feature

---
+++ src/components/scrollToTop.tsx
@@ -0,0 +1,49 @@
+import { makeStyles, Theme, createStyles } from "@material-ui/core/styles";
+import Fab from "@material-ui/core/Fab";
+import useScrollTrigger from "@material-ui/core/useScrollTrigger";
+import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
+import Zoom from "@material-ui/core/Zoom";
+
+interface Props {
+  children: React.ReactElement;
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      position: "fixed",
+      bottom: theme.spacing(3),
+      right: theme.spacing(3),
+    },
+  })
+);
+
+function ScrollTop({ children }: Props) {
+  const classes = useStyles();
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 700,
+  });
+
+  const handleClick = () => {
+    document.documentElement.scrollTop = 0;
+  };
+
+  return (
+    <Zoom in={trigger}>
+      <div onClick={handleClick} role="presentation" className={classes.root}>
+        {children}
+      </div>
+    </Zoom>
+  );
+}
+
+const ScrollToTop = () => (
+  <ScrollTop>
+    <Fab color="secondary" size="small" aria-label="scroll back to top">
+      <KeyboardArrowUpIcon />
+    </Fab>
+  </ScrollTop>
+);
+
+export default ScrollToTop;
