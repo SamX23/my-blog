43389b2aa4c8fa4ec26fae914cc510134601d99d
samx23 <skalexsong@gmail.com>
2021-05-01 20:22:33 +0700

    Create : add about pages

---
+++ pages/about.tsx
@@ -0,0 +1,14 @@
+import Grid from "@material-ui/core/Grid";
+import { Layout } from "../src/components";
+
+const about = () => (
+  <Layout>
+    <Grid container>
+      <Grid item>
+        <h1>About Page</h1>
+      </Grid>
+    </Grid>
+  </Layout>
+);
+
+export default about;
