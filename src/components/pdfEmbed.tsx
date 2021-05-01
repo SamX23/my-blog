475b02e6de9eeb56f274e04c02da7b2671ea443d
samx23 <skalexsong@gmail.com>
2021-05-01 20:53:51 +0700

    Update : change border

--- src/components/pdfEmbed.tsx
+++ src/components/pdfEmbed.tsx
@@ -2,8 +2,8 @@ import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 
 const EmbedPdf = () => (
-  <Box borderTop={2} mt={2}>
-    <Box mb={2} textAlign="center">
+  <Box mt={2}>
+    <Box mb={2} pb={2} borderBottom={2} textAlign="center">
       <Typography variant="h4">My Resume</Typography>
     </Box>
     <embed
