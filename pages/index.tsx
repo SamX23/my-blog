9847799d28fb0a57d7d59efa1139fd715c5ac3c5
SamX23 <skalexsong@gmail.com>
2022-02-02 22:48:32 +0700

    Update : eslint rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,27 +20,24 @@ const Home = () => {
   const classes = useStyles();
 
   return (
-    <>
-      <Layout home>
-        <Grid container className={classes.container}>
-          <Grid item>
-            <Typography
-              variant="body1"
-              className={classes.typography}
-              id="content"
-            >
-              Currently a Front-End Developer at Utas.Co, loving the most fast
-              paced tech stack on earth,{" "}
-              <a href="https://reactjs.org/">React</a>. Found my self enjoy
-              coding and develop things, this blog is created using{" "}
-              <a href="https://nextjs.org/">Next</a> and{" "}
-              <a href="https://www.typescriptlang.org/">Typescript</a>. Checkout
-              my personal project on <a href="/about">Github</a>.
-            </Typography>
-          </Grid>
+    <Layout home>
+      <Grid container className={classes.container}>
+        <Grid item>
+          <Typography
+            variant="body1"
+            className={classes.typography}
+            id="content"
+          >
+            Currently a Front-End Developer at Utas.Co, loving the most fast
+            paced tech stack on earth, <a href="https://reactjs.org/">React</a>.
+            Found my self enjoy coding and develop things, this blog is created
+            using <a href="https://nextjs.org/">Next</a> and{" "}
+            <a href="https://www.typescriptlang.org/">Typescript</a>. Checkout
+            my personal project on <a href="/about">Github</a>.
+          </Typography>
         </Grid>
-      </Layout>
-    </>
+      </Grid>
+    </Layout>
   );
 };
 
