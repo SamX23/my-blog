26d50b2e0ae27026de5ed10c4d12f1134cee7916
Sami Kalammallah <skalexsong@gmail.com>
2023-12-25 09:29:13 +0700

    chore: update introduction text

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,15 +29,18 @@ const Home = () => {
             className={classes.typography}
             id="content"
           >
-            Currently a Software Engineer focused on frontend role, loving the
-            most fast paced tech stack on earth,{" "}
+            As a dedicated Software Engineer specializing in frontend
+            development, I am particularly drawn to the dynamic and fast-paced
+            world of technology, with{" "}
             <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
               React
-            </a>
-            . Found my self enjoy coding, playing games and being a father, this
-            blog is created using{" "}
+            </a>{" "}
+            as my primary focus. I derive immense satisfaction from coding,
+            engaging in gaming pursuits, and fulfilling my responsibilities as a
+            father. The creation of this blog is a testament to my proficiency
+            in utilizing{" "}
             <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-              Next
+              Next Js
             </a>{" "}
             and{" "}
             <a
@@ -47,7 +50,9 @@ const Home = () => {
             >
               Typescript
             </a>
-            . Checkout my personal project on <Link href="/about">Github</Link>.
+            . Feel free to explore my personal projects on{" "}
+            <Link href="/about">Github</Link> for a deeper insight into my
+            coding endeavors.
           </Typography>
         </Grid>
       </Grid>
